e3d0d6747338fb83a937959dfbf3d15455b157bc
Ali Soner Bozkurt <74866543+alisonerboz@users.noreply.github.com>
2023-07-30 21:45:40 +0300

    Update

--- Assets/Standard Assets/Vehicles/Car/Scripts/CarAudio.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarAudio.cs
@@ -87,7 +87,7 @@ namespace UnityStandardAssets.Vehicles.Car
         private void Update()
         {
             // get the distance to main camera
-            float camDist = (Camera.main.transform.position - transform.position).sqrMagnitude;
+            float camDist =  (GameObject.Find("Camera").gameObject.transform.position - transform.position).sqrMagnitude;
 
             // stop sound if the object is beyond the maximum roll off distance
             if (m_StartedSound && camDist > maxRolloffDistance*maxRolloffDistance)
